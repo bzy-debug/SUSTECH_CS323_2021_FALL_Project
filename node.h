4c5509902c6f37255c26b8d1f76677a356dad658
bzy <823324993@qq.com>
2021-10-01 21:28:02 +0800

    construct node in lex.l

--- node.h
+++ node.h
@@ -1,6 +1,27 @@
+typedef enum NODE_TYPE {
+    eID, eTYPE, eINT, eFLOAT, eCHAR, nterm,
+    eSTRUCT, eIF, eELSE, eWHILE, eRETURN,
+    eDOT, eSEMI, eCOMMA, eASSIGN,
+    eLT, eLE, eGT, eGE, eNE, eEQ, ePLUS, eMINUS, eMUL, eDIV,
+    eAND, eOR, eNOT, eLP, eRP, eLB, eRB, eLC, eRC
+} nodeType;
+
+typedef union NODE_VAL
+{
+    int intval;
+    float floatval;
+    char charval;
+    char* typeval;
+    char* idval;
+    char* ntermval;
+}nodeVal;
+
+
 typedef struct NODE
 {
-    char* info;
+    nodeType node_type;
+    int isempty; // 0 empty, 1 empty. for nterms
+    nodeVal val;
     struct NODE* child;
     struct NODE* sibling;
 } node;
