f831b0262ab118bfa0ea3126ae2460f6c8ddf65d
wuw584 <78073129+wuw584@users.noreply.github.com>
2021-12-10 16:11:13 +0800

    Update inter_code.h

--- project3/inter_code.h
+++ project3/inter_code.h
@@ -1,59 +1,15 @@
-#ifndef __INTER_CODE_H
-#define __INTER_CODE_H
-
-typedef struct operand_ operand;
-typedef struct inter_code_ inter_code;
-typedef enum operand_kind_ op_kind;
-typedef enum code_kind_ ic_kind;
-typedef enum relop_ relop;
-
-enum operand_kind_ { VARIABLE, CONSTANT, LABEL, FUNCTION } ;
-
-enum code_kind_{ 
-    DEF_LAB, DEF_FUNC, ASSIGN, ADD, SUB, MUL, DIV,
-    ADDR, GOTO, IF, RETURN, DEC, ARG, CALL, PARAM,
-    READ, WRITE, LEFT_S, RIGHT_S
+#ifndef __LLIST_NODE_H
+#define __LLIST_NODE_H
+typedef struct _llist_node llist_node;
+struct _llist_node
+{
+    char* key;
+    void* value;
+    llist_node* next;
+    llist_node* prev;
 };
 
-enum relop_ {
-    EQ, NE, LT, GT, LE, GE
-};
+llist_node* create_node(char* key, void* value);
 
-struct operand_ {
-    op_kind kind;
-    union 
-    {
-        char* var_name;
-        int value;
-        int label_no;
-        char* func_name;
-    } u;
-};
-
-struct inter_code_ {
-    ic_kind kind;
-    union {
-        struct { operand* op; } one_op;
-        struct { operand *right, *left; } assign;
-        struct { operand *result, *op1, *op2; } binop;
-        struct { operand *result, *op1, *op2; relop rop;} ifgo;
-    } u;    
-};
-
-operand* cnt_op_int(op_kind kind, int op_val);
-
-operand* cnt_op_str(op_kind kind, char* op_val);
-
-inter_code* cnt_ic(ic_kind kind, int num, ...);
-
-void print_code(inter_code* code);
-
-void print_op(operand* op);
-
-char* relop_to_string(relop rop);
-
-char* op_to_string(operand* op);
-
-char* code_to_string(inter_code* code);
-
+void free_node(llist_node* node, void (*free_key)(void *), void (*free_value)(void *));
 #endif
